a7af73bb8ae04f05eee8ca47b369e1dd42bb824a
ben wainaina <bwainainamwangi@gmail.com>
2024-11-11 11:50:04 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '26.using-css-sprites-in-angular-to-reduce-assets-fetch-time';
+}
